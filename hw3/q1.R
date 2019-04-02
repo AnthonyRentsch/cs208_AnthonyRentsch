4388082eff495d9856ae22bea26725285ed52ed1
Anthony Rentsch <anthony.rentsch@gmail.com>
2019-04-02 00:34:05 -0400

    nearly finished with hw3, just need to do 1e

--- hw3/q1.R
+++ hw3/q1.R
@@ -22,7 +22,7 @@ trimmedMean <- function(x, d, epsilon) {
   n <- length(x)
   scale <- d/(epsilon*0.9*n)
   quants <- quantile(x, c(0.05,0.95))
-  x_trimmed <- x[x>quants[1] && x<quants[2]]
+  x_trimmed <- x[x>quants[1] & x<quants[2]]
   mean_trimmed <- (1/(0.9*n))*sum(x_trimmed)
   mean_release <- mean_trimmed + rlap(mu=0,b=scale,size=1)
   return(mean_release)
@@ -36,8 +36,9 @@ exponentialPercentile <- function(x, t, epsilon){
 
   likelihoods <- rep(NA, nbins)
   for(i in 1:nbins){
-    quality <- nbins - abs(nbins*t - i)
-    bin_width <- ifelse((i+1) %in% 1:nbins, bins[i]-bins[i+1]+1, 1)
+    quality <- -1 * abs(nbins*t - i)
+    if(i < nbins) { bin_width <- bins[i]-bins[i+1]+1 }
+    else { bin_width <- 1 }
     likelihoods[i] <- (exp(epsilon * quality) / 2)*(bin_width)
   }
   
@@ -58,7 +59,7 @@ e3trimmedMean <- function(x, epsilon, tile_low=5, tile_high=95) {
   
   tile_low_value <- exponentialPercentile(x, t=tile_low, epsilon=epsilon/3)
   tile_high_value <- exponentialPercentile(x, t=tile_high, epsilon=epsilon/3)
-  x_trimmed <- x[x>tile_low_value && x<tile_high_value]
+  x_trimmed <- x[x>tile_low_value & x<tile_high_value]
   mean_trimmed <- (1/(0.9*n))*sum(x_trimmed)
   
   scale <- (3*(tile_high_value-tile_low_value))/(0.9*epsilon*n)
@@ -75,8 +76,8 @@ clip <- function(x, lower, upper){     # borrowed from class
 }
 
 rmse <- function(pred, true) {
-  rmse <- sqrt(mean((pred-true)^2))
-  return(rmse)
+  val <- sqrt(mean((pred-true)^2))
+  return(val)
 }
 
 laplaceMeanRelease <- function(x, lower, upper, epsilon){     # borrowed from class
@@ -91,24 +92,70 @@ laplaceMeanRelease <- function(x, lower, upper, epsilon){     # borrowed from cl
   return(DPrelease)
 }
 
-x = pums
-pumas <- unique(x$puma)
-num_pumas <- length(pumas)
+### simulations
+pumas <- unique(pums$puma)
+n_pumas <- length(pumas)
+n_reps <- 100
+means_mat <- matrix(NA, nrow=n_pumas*n_reps, ncol=6)
+row_ind <- 1
 
-means <- rep(NA, num_pumas)
-lapmean_rels = rep(NA, num_pumas)
-e3trimmedmean_rels = rep(NA, num_pumas)
-rmses <- rep(NA, num_pumas)
-
-for(i in 1:num_pumas){
+for(i in 1:n_pumas){
   print(i)
-  means[i] <- mean(x$income[x$puma==pumas[i]])
-  lapmean_rels[i] <- laplaceMeanRelease(x=x$income[x$puma==pumas[i]],lower=0, upper=100000, epsilon=0.5)
-  e3trimmedmean_rels[i] <- e3trimmedMean(x=x$income[x$puma==pumas[i]], epsilon=0.5)
-  rmses[i] <- rmse(means[i], lapmean_rels[i])
+  puma_region <- pumas[i]
+  dat <- pums$income[pums$puma==puma_region]
+  true_mean <- mean(dat)
+  
+  for(j in 1:n_reps){
+    laplace_noise <- laplaceMeanRelease(x=dat, lower=0, upper=1000000 , epsilon=1)
+    e3_trimmed <- e3trimmedMean(x=dat, epsilon=1)
+    means_mat[row_ind,1] <- puma_region
+    means_mat[row_ind,2] <- true_mean
+    means_mat[row_ind,3] <- laplace_noise
+    means_mat[row_ind,4] <- e3_trimmed
+    means_mat[row_ind,5] <- rmse(laplace_noise, true_mean)
+    means_mat[row_ind,6] <- rmse(e3_trimmed, true_mean)
+    row_ind <- row_ind + 1
+  }
 }
 
-plot(density(lapmean_rels), col="blue")
-lines(density(e3trimmedmean_rels), col="red")
-lines(density(means), col="green")
+means_df <- data.frame(means_mat)
+names(means_df) <- c("puma","true_mean","laplace_mean","exponential_mean","rmse_laplace","rmse_exponential")
+
+### results
+q1_results <- means_df %>% group_by(puma) %>% summarise(avg_lap=mean(rmse_laplace),
+                                          avg_exp=mean(rmse_exponential))
+write.csv(q1_results, "q1_results.csv")
+
+
+q1_plot1 <- ggplot(data=means_df, aes(x=reorder(factor(puma), -laplace_mean))) + 
+  geom_boxplot(aes(y=laplace_mean, shape="DP means"), outlier.shape=NA, alpha=0.7) + 
+  geom_point(aes(y=true_mean, colour="True mean")) +
+  scale_colour_manual(values=c("red")) +
+  labs(x="PUMA region", y="Mean income", title="Laplace mechanism") + 
+  theme_bw() + 
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank(), axis.ticks.y=element_blank(),
+        axis.line = element_line(colour = "black"), panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), panel.border = element_blank(), 
+        panel.background = element_blank(), legend.title = element_blank(),
+        plot.title = element_text(hjust = 0.5))
+pdf("plots/q1_plot1.pdf", width=8, height=8)
+q1_plot1
+dev.off()
+
+
+q1_plot2 <- ggplot(data=means_df, aes(x=reorder(factor(puma), -trimmed_mean))) + 
+  geom_boxplot(aes(y=trimmed_mean, shape="DP means"), outlier.shape=NA, alpha=0.7) + 
+  geom_point(aes(y=true_mean, colour="True mean"), alpha=0.7) +
+  scale_colour_manual(values=c("red")) +
+  labs(x="PUMA region", y="Mean income", title="Trimmed exponential mechanism") + 
+  theme_bw() + 
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank(), axis.ticks.y=element_blank(),
+        axis.line = element_line(colour = "black"), panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), panel.border = element_blank(), 
+        panel.background = element_blank(), legend.title = element_blank(),
+        plot.title = element_text(hjust = 0.5))
+pdf("plots/q1_plot2.pdf", width=8, height=8)
+q1_plot2
+dev.off()
+
 
